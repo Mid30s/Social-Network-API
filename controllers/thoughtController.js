9693ce483da2a1a2626b145d76a3945c7a61fb5f
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-04-22 13:16:42 +0930

    added parts of user controller and code structure of thought controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,24 @@
+const { Thought, User } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  async getAllThoughts(req, res) {},
+
+  // Get a single thought by ID
+  async getThoughtById(req, res) {},
+
+  // Create a new thought
+  async createThought(req, res) {},
+
+  // Update a thought by ID
+  async updateThought(req, res) {},
+
+  // Delete a thought by ID
+  async deleteThought(req, res) {},
+
+  // Create a reaction for a thought
+  async createReaction(req, res) {},
+
+  // Remove a reaction from a thought
+  async deleteReaction(req, res) {},
+};
---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Get a single user by ID
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId }).select(
+        "-__v"
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Delete a user by ID
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json({ message: "User deleted!" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  //update a user by ID
+  async updateUser(req, res) {},
+
+  //add a friend to a user's friend list
+  async addFriend(req, res) {},
+
+  //remove a friend from a user's friend list
+  async removeFriend(req, res) {},
+};
